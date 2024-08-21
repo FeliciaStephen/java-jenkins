96a2f9e247e6b50cb362d8cb6369e754b7480818
FeliciaStephen <145844002+FeliciaStephen@users.noreply.github.com>
2024-08-21 13:59:29 +0530

    Update jenkin.java

--- jenkin.java
+++ jenkin.java
@@ -4,8 +4,8 @@ public class jenkin
 public static void main(String args[])
 {
 Scanner sc = new Scanner(System.in);
-System.out.println("Enter your name:");
-String name=sc.next();
-System.out.println("Welcome "+name);
+// System.out.println("Enter your name:");
+// String name=sc.next();
+System.out.println("Welcome ");
 }
 }
